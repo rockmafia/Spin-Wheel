34e29c8ab0ebe35e82f7a8586cd6665e82087e84
Rock <rocky_oo@hotmail.com>
2024-10-29 15:57:41 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -7,33 +7,22 @@ import GoldenWheelSpin from "./GoldenWheelSpin.jsx";
 import SpinWheelVerone from "./SpinWheelVerone.jsx";
 import "./index.css";
 import PeopleWheelSpin from "../src/PeopleWheelSpin.jsx";
-import { BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter, } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      {/* <App /> */}
-
       <Routes>
         <Route
           path="/"
           element={
             <>
-              {" "}
-              <SpinWheelVerone /> <GoldenWheelSpin />{" "}
+              <SpinWheelVerone /> <GoldenWheelSpin />
             </>
           }
         />
         <Route path="/Peoplegold" element={<PeopleWheelSpin />} />
-
-        {/* <SpinWheelVerone/> 
-    {/* <PrizeWheelLast/> */}
-        {/* <GoldenWheelSpin/>  */}
       </Routes>
     </StrictMode>
-    ,
   </BrowserRouter>
 );
