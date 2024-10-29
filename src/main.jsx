854ac68a9c2c8f74b11ece327b5f1935efe30ef8
Rock <rocky_oo@hotmail.com>
2024-10-29 15:55:30 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import PrizeWheel from './PrizeWheel.jsx'
-import PrizeWheelLast from './PrizeWheelLast.jsx'
-import GoldenWheelSpin from './GoldenWheelSpin.jsx'
-import SpinWheelVerone from './SpinWheelVerone.jsx'
-import './index.css'
-import PeopleWheelSpin from "../src/PeopleWheelSpin.jsx"
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import PrizeWheel from "./PrizeWheel.jsx";
+import PrizeWheelLast from "./PrizeWheelLast.jsx";
+import GoldenWheelSpin from "./GoldenWheelSpin.jsx";
+import SpinWheelVerone from "./SpinWheelVerone.jsx";
+import "./index.css";
+import PeopleWheelSpin from "../src/PeopleWheelSpin.jsx";
+import { BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter, } from "react-router-dom";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-      
-   
-  <StrictMode >
-    {/* <App /> */}
-   
-   <Routes>
-    <Route path='/' element={<> <SpinWheelVerone/> <GoldenWheelSpin/> </> }/>
-    <Route path="/peoplegold" element={<PeopleWheelSpin />} />
-  
-   {/* <SpinWheelVerone/> 
-    {/* <PrizeWheelLast/> */}
-    {/* <GoldenWheelSpin/>  */}
-   </Routes>
+    <StrictMode>
+      {/* <App /> */}
 
-  </StrictMode>,
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              {" "}
+              <SpinWheelVerone /> <GoldenWheelSpin />{" "}
+            </>
+          }
+        />
+        <Route path="/Peoplegold" element={<PeopleWheelSpin />} />
+
+        {/* <SpinWheelVerone/> 
+    {/* <PrizeWheelLast/> */}
+        {/* <GoldenWheelSpin/>  */}
+      </Routes>
+    </StrictMode>
+    ,
   </BrowserRouter>
-)
+);
